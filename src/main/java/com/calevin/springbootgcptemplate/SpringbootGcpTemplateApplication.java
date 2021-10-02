aee513271fccbdadf44d449417cf9fe65721dc33
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-10-02 19:50:51 -0300

    Add Spring Boot project files

---
+++ src/main/java/com/calevin/springbootgcptemplate/SpringbootGcpTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.calevin.springbootgcptemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGcpTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGcpTemplateApplication.class, args);
+	}
+
+}
