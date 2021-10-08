5877468d5613ae4166dce5e9a2e27597804d50ec
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-10-08 17:57:44 -0300

    Add logging examples

--- src/main/java/com/calevin/springbootgcptemplate/SpringbootGcpTemplateApplication.java
+++ src/main/java/com/calevin/springbootgcptemplate/SpringbootGcpTemplateApplication.java
@@ -1,10 +1,12 @@
 package com.calevin.springbootgcptemplate;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @SpringBootApplication
 public class SpringbootGcpTemplateApplication {
 
@@ -16,6 +18,11 @@ public class SpringbootGcpTemplateApplication {
 	class HelloWorldController {
 		@GetMapping("/")
 		public String hello() {
+			log.trace("A TRACE Message");
+			log.debug("A DEBUG Message");
+			log.info("An INFO Message");
+			log.warn("A WARN Message");
+			log.error("An ERROR Message");
 			return "hello world!";
 		}
 	}
