0e3cebf18e0396e7cb647e5dbbedffaf8d4a92f4
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-10-13 19:21:02 -0300

    Add Repository annotation

--- src/main/java/com/calevin/springbootgcptemplate/repositories/CategoryExampleRepository.java
+++ src/main/java/com/calevin/springbootgcptemplate/repositories/CategoryExampleRepository.java
@@ -2,6 +2,8 @@ package com.calevin.springbootgcptemplate.repositories;
 
 import com.calevin.springbootgcptemplate.entities.CategoryExample;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryExampleRepository extends JpaRepository<CategoryExample, Long> {
 }
--- src/main/java/com/calevin/springbootgcptemplate/repositories/EntityExampleRepository.java
+++ src/main/java/com/calevin/springbootgcptemplate/repositories/EntityExampleRepository.java
@@ -2,6 +2,8 @@ package com.calevin.springbootgcptemplate.repositories;
 
 import com.calevin.springbootgcptemplate.entities.EntityExample;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EntityExampleRepository extends JpaRepository<EntityExample, Long> {
 }
