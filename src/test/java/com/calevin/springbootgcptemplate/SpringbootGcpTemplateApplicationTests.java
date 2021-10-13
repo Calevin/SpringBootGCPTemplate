59985d49138449214e0e6b3a0f64757686d9e5a4
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-10-13 19:21:27 -0300

    Update contextLoads test

--- src/test/java/com/calevin/springbootgcptemplate/SpringbootGcpTemplateApplicationTests.java
+++ src/test/java/com/calevin/springbootgcptemplate/SpringbootGcpTemplateApplicationTests.java
@@ -1,13 +1,37 @@
 package com.calevin.springbootgcptemplate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import com.calevin.springbootgcptemplate.controllers.EntityExampleController;
+import com.calevin.springbootgcptemplate.repositories.CategoryExampleRepository;
+import com.calevin.springbootgcptemplate.repositories.EntityExampleRepository;
+import com.calevin.springbootgcptemplate.services.EntityExampleService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootGcpTemplateApplicationTests {
 
+	@Autowired
+	private EntityExampleController entityExampleController;
+
+	@Autowired
+	private EntityExampleRepository entityExampleRepository;
+
+	@Autowired
+	private CategoryExampleRepository categoryExampleRepository;
+
+	@Autowired
+	private EntityExampleService entityExampleService;
+
 	@Test
 	void contextLoads() {
+		assertThat(entityExampleController).isNotNull();
+		assertThat(entityExampleRepository).isNotNull();
+		assertThat(categoryExampleRepository).isNotNull();
+		assertThat(entityExampleService).isNotNull();
 	}
 
 }
